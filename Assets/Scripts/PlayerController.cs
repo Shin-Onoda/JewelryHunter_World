71a7a52be1d9851fb322637eecf7f9e287229c58
MAIN2024\works <254623536+Shin-Onoda@users.noreply.github.com>
2026-02-02 15:35:12 +0900

    JewelryHunter_World Unityデータ追加

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+// Start is called once before the first execution of Update after the MonoBehaviour is created
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rbody;          //Rigidbody2D型の変数
+    float axisH = 0.0f;         //入力
+
+    void Start()
+    {
+        //Rigidbody2Dを取ってくる
+        rbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //水平方向の入力をチェックする
+        axisH = Input.GetAxisRaw("Horizontal");
+    }
+    
+    private void FixedUpdate()
+    {
+        //速度を更新する
+        rbody.linearVelocity = new Vector2(axisH * 3.0f, rbody.linearVelocity.y);
+    }
+}
