1e27066874f7a8a694643f1d9c0af11065fdab64
MAIN2024\works <254623536+Shin-Onoda@users.noreply.github.com>
2026-02-09 14:26:56 +0900

    ゲーム進行系UIの追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum GameState   //ゲームの状態
 {
@@ -15,6 +16,7 @@ public class GameManager : MonoBehaviour
 {
     //ゲームの状態
     public static GameState gameState;
+    public string nextSceneName;            // 次のシーン名
     void Start()
     {
         gameState = GameState.InGame;   //インゲームにする
@@ -24,5 +26,17 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //リスタート
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    //次へ
+    public void Next()
+    {
+        SceneManager.LoadScene(nextSceneName);
     }
 }
