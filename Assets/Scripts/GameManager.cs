33ad6c7b25c7e7ca968bb66fc09daa28e35e5191
MAIN2024\works <254623536+Shin-Onoda@users.noreply.github.com>
2026-02-16 11:18:10 +0900

    sound追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,9 +20,16 @@ public class GameManager : MonoBehaviour
 
     //スコア追加
     public static int totalScore;           //合計スコア
+
+    //サウンド関連
+    public AudioClip meGameClear;
+    public AudioClip meGameOver;
+    AudioSource soundPlayer;
+
     void Start()
     {
         gameState = GameState.InGame;   //インゲームにする
+        soundPlayer = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -30,10 +37,14 @@ public class GameManager : MonoBehaviour
     {
         if(gameState == GameState.GameClear)
         {
+            soundPlayer.Stop();
+            soundPlayer.PlayOneShot(meGameClear);
             gameState = GameState.GameEnd;
         }
         else if(gameState == GameState.GameOver)
         {
+            soundPlayer.Stop();
+            soundPlayer.PlayOneShot(meGameOver);
             gameState = GameState.GameEnd;
         }
     }
